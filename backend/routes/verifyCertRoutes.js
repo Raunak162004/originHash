d7631389ee1b47d17a49cd6c073838d89fbbf8d2
Lavi sharma <lavipandit123@gmail.com>
2025-08-10 22:16:42 +0530

    implemented a middleware usermiddleware for payement and verify route

--- backend/routes/verifyCertRoutes.js
+++ backend/routes/verifyCertRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { verifyCertificate, confirmPaymentAndVerify } from "../controller/verifyCertController.js";
 import { isLoggedIn } from "../middlewares/userAuthMiddleware.js";
+import { userProtect } from "../middlewares/userMiddleware.js";
 
 const router = express.Router();
 
 // Step 1: Verify by Certificate ID
-router.post("/verify", verifyCertificate);
+router.post("/verify", userProtect, verifyCertificate);
 
 // Step 2: Dummy Payment → Mark as verified
-router.post("/verify/payment", confirmPaymentAndVerify);
+router.post("/verify/payment", userProtect, confirmPaymentAndVerify);
 
 export default router;
